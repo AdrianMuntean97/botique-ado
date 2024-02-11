5d2f0bc69e00e2cf99022c585ac81ab4ecdce6fb
AdrianMuntean97 <adi_muntean_13@yahoo.com>
2024-02-11 14:42:28 +0000

    Added stripe elements.js

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Oie0ZEdEc8PFYhZUR2gSh266AolGDJ2fL3309SOgkYNtI8xgstYngJZZ4QKY6wMD7AGzV7HQPhDO5hJ1dWsI3uj00CZm4jv3I',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
