a40896aac5c9ecbcd444ee8aff7ececa81fea537
AdrianMuntean97 <adi_muntean_13@yahoo.com>
2024-02-12 13:25:49 +0000

    completed webhook handler

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-adrianmuntea-botiqueado-f8fvso4nd4o.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['8000-adrianmuntea-botiqueado-944ummf4n1f.ws-eu108.gitpod.io']
 
 
 # Application definition
